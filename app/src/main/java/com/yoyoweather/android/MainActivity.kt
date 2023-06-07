6fc931723f27712dca0ebcd7f3ecbcd24fc02a35
2a588 <goodecba@gmail.com>
2023-06-07 11:39:05 +0800

    add  hf api

--- app/src/main/java/com/yoyoweather/android/MainActivity.kt
+++ app/src/main/java/com/yoyoweather/android/MainActivity.kt
@@ -2,10 +2,14 @@ package com.yoyoweather.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //设置状态栏沉浸
+         WindowCompat.setDecorFitsSystemWindows(window,false)
     }
 }
--- app/src/main/java/com/yoyoweather/android/YoyoweatherApplication.kt
+++ app/src/main/java/com/yoyoweather/android/YoyoweatherApplication.kt
@@ -6,10 +6,14 @@ import android.content.Context
 
 class YoyoweatherApplication : Application() {
     companion object {
-
         const val TOKEN = "CAIYUNTOKEN1234"
         @SuppressLint("StaticFieldLeak")
         lateinit var context: Context
+
+        const val TOKENHF = "TEST"  //和风天气apitoken
+
+        //生活指数类型
+        const val INDECES_TYPE = "1,2,5"
     }
 
     override fun onCreate() {
